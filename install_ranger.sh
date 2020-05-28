d902242be091fa8bc3ee6e1f453e81e72ebb6591
teresi <miketeresi@gmail.com>
2020-05-28 13:29:31 -0400

    add ranger config setup

--- install_ranger.sh
+++ install_ranger.sh
@@ -9,13 +9,15 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # ubuntu dependencies
 sudo apt-get install libx11-dev libxext-dev
-sudo apt-get install ranger
 python3 -m pip install --user ueberzug
 
 # need to run ranger in xvrt for iimages
 sudo apt-get install rxvt-unicode xsel
-
-# sane defaults
-cp $ROOT_DIR/Xresources /home/`whoami`/.Xresources
+cp $ROOT_DIR/Xresources /home/`whoami`/.Xresources  # sane defaults
 xrdb -merge ~/.Xresources
 
+# ranger w/ sane defaults
+sudo apt-get install ranger
+ranger --copy-config=all
+sed -i 's/set\ preview_images\ false/set\ preview_images\ true/' ~/.config/ranger/rc.conf
+sed -i 's/set\ preview_images_method\ w3m/set\ preview_images_method\ urxvt/' ~/.config/ranger/rc.conf
