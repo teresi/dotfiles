9a695e1c019bad51f2e946a996cff0ee16051c91
Michael Teresi <michael.teresi@navy.mil>
2022-05-04 18:07:34 -0400

    fix vundle

--- install_vundle.bash
+++ install_vundle.bash
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 # download vundle for vim package management
+# Args:
+#    $1: destination to download vundle to
 
 _ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $_ROOT_DIR/helpers.bash
 notify "download VUNDLE..."
 
-VUNDLE_DEST="$(HOME)/.vim/bundle/Vundle.vim"
+VUNDLE_DEST="$HOME/.vim/bundle/Vundle.vim"
 VUNDLE_GIT_URL=https://github.com/VundleVim/Vundle.vim.git
-
-git clone https://github.com/VundleVim/Vundle.vim.git $(HOME)/.vim/bundle/Vundle.vim
 
 if [ $# -eq 1 ] && [ ! -z "$1" ]; then
 	VUNDLE_DEST="$1"
 fi
 
-update_repo_to_master "$FZF_GIT_URL" "$FZF_DEST"
-"$FZF_DEST"/install --all
+update_repo_to_master "$VUNDLE_GIT_URL" "$VUNDLE_DEST"
