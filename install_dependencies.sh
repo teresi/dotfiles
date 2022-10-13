782483c0306deb5c9fc239d548aeebd7518299ad
Michael Teresi <miketeresi@gmail.com>
2022-10-13 12:45:10 -0400

    update dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,16 +4,17 @@
 # for generic packages that aren't tied to an install script
 
 
-set -ex
+set -x
 echo -e "\e[32mINFO  install OS dependencies...\e[39m"
 
 
 # NB required for setting up our configuration
 echo -e "\e[32mINFO      install compile-time dependencies...\e[39m"
-sudo apt-get install git                          # for downloading dependencies
-sudo apt-get install git-lfS                      # for downloading dependencies
-sudo apt-get install python3-pip                  # for downloading dependencies
+sudo apt install git git-lfs                  # for downloading dependencies
+sudo apt install python3-pip                  # for downloading dependencies
 
+
+sudo apt install gir1.2-gtop-2.0              # for multi-core system monitor (cinnamon applet / jaszhix)
 
 # NB required for running our configuration
 echo -e "\e[32mINFO      install run-time dependencies...\e[39m"
