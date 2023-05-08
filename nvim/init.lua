0e2783af2ea15ed7e86a9cd3ad3281b5c9af7dbb
Michael Teresi <miketeresi@gmail.com>
2023-05-07 22:19:26 -0400

    don't use tab for neovim next/prev code complete

--- nvim/init.lua
+++ nvim/init.lua
@@ -493,7 +493,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<Down>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -502,7 +502,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<Up>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
