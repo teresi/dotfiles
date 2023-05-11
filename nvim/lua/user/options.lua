81c934312f15e700736e3a8ea42699a85ac34752
Michael Teresi <miketeresi@gmail.com>
2023-05-10 21:09:20 -0400

    nvim refactoring

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,13 +1,41 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
 
 -- SEE  https://github.com/LunarVim/nvim-basic-ide/blob/master/lua/options.lua
---
+
+
 vim.opt.number = number          -- show line numbers
 vim.opt.relativenumber = true    -- show line number relative to current line
 vim.opt.cursorline = true        -- highlight current line
 
 
 vim.opt.titlestring = [[%f %h%m%r%w %{v:progname} (%{tabpagenr()} of %{tabpagenr('$')})]]
-vim.opt.hlsearch = true
 
+vim.opt.hlsearch = true          -- highlight matches on search
 vim.opt.termguicolors = true     -- set termguicolors to enable highlight groups
 
+vim.opt.mouse = 'a'                -- enable mouse
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.breakindent = true         -- wrapped lines are visually indented
+
+vim.opt.undofile = true            -- persistent undo history
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'          -- show lint/debug signs on left hand side
+
+vim.opt.updatetime = 1000            -- period to write swap file (default 40000)
+
+vim.opt.timeout = true               -- enable to wait `ttimeoutlen` after recieving ESC for another key
+
+vim.opt.timeoutlen = 300             -- ms to wait for a mapped sequence to complete (default 1000)
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = 'menuone,noselect'
