5596a7391681eebb5e9c12d8b923925fbb8dc6ea
Michael Teresi <miketeresi@gmail.com>
2024-01-04 12:56:22 -0500

    fix lua lsp, unknown global 'vim'
    - fix issue where lua gives warnings about nvim configs,
      b/c it doesn't have knowledge of nvim,
      by adding the nvim runtime
    - SEE https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#luau_lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -31,10 +31,39 @@ return {
     },
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
       lspconfig.rust_analyzer.setup({})
       lspconfig.autotools_ls.setup({})
+      require'lspconfig'.lua_ls.setup {
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+              Lua = {
+                runtime = {
+                  -- Tell the language server which version of Lua you're using
+                  -- (most likely LuaJIT in the case of Neovim)
+                  version = 'LuaJIT'
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                  }
+                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                  -- library = vim.api.nvim_get_runtime_file("", true)
+                }
+              }
+            })
+
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          end
+          return true
+        end
+      }
       -- Global mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
