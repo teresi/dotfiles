1a3cc84ba1b3c01ce363b1315c50b283123a9592
Michael Teresi <miketeresi@gmail.com>
2024-02-26 11:55:46 -0500

    add initial nvim autocompletion

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v2.x",
+  dependencies = {
+    { "neovim/nvim-lspconfig" }, -- Required
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "L3MON4D3/Luasnip" }, -- Required
+    { "rafamadriz/friendly-snippets" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "saadparwaiz1/cmp_luasnip" },
+  }
+}
