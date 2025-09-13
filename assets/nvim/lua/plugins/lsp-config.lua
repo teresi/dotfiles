b78f0d960b9b273a3d02aa8c5adc2e8ae1faffa5
Michael Teresi <miketeresi@gmail.com>
2025-09-12 20:20:54 -0400

    add markdownlint
    - fix issue on markdown files, linter missing,
      markdownlint ENOENT (error no entry)

--- assets/nvim/lua/plugins/lsp-config.lua
+++ assets/nvim/lua/plugins/lsp-config.lua
@@ -250,6 +250,8 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format Lua code
+			"markdownlint-cli2",
+			"markdownlint",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
