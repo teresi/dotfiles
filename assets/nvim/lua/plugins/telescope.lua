f5af921cfaed86ae466e69f15e6f4d3633f87738
Michael Teresi <miketeresi@gmail.com>
2025-10-22 19:54:07 -0400

    update fuzzy find in buffer w/ larger display (leader /)

--- assets/nvim/lua/plugins/telescope.lua
+++ assets/nvim/lua/plugins/telescope.lua
@@ -84,9 +84,9 @@ return {
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_ivy({
+				winblend = 5,
+				previewer = true,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
 
