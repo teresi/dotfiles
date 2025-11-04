8fdfd9f203554dd6f3d421dac7e504ff63f4839c
Michael Teresi <miketeresi@gmail.com>
2025-11-04 00:07:43 -0500

    pull tags on clone
    - fix helper function where tags were fetched on a clone
      but were in subsequent fetches

--- helpers.bash
+++ helpers.bash
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
 
-
 # bash functions
 
-
-notify () {
-	# info message, green text prepended w/ "INFO  "
-	echo -e "\e[32mINFO\t$1 \e[39m"
+notify() {
+    # info message, green text prepended w/ "INFO  "
+    echo -e "\e[32mINFO\t$1 \e[39m"
 }
 
-
-warn () {
-	# warning message, yellow text prepended w/ "WARN  "
-	echo -e "\e[33mWARN\t$1 \e[39m"
+warn() {
+    # warning message, yellow text prepended w/ "WARN  "
+    echo -e "\e[33mWARN\t$1 \e[39m"
 }
 
-
-error () {
-	# error message, red text prepended w/ "ERROR  "
-	echo -e "\e[91mERROR\t$1 \e[39m"
+error() {
+    # error message, red text prepended w/ "ERROR  "
+    echo -e "\e[91mERROR\t$1 \e[39m"
 }
 
-
-update_repo_to_master () {
+update_repo_to_master() {
     # clone | fetch | reset to bring a git repo up to date
     #
     #	1	repo url
@@ -46,16 +41,15 @@ update_repo_to_master () {
     notify "    repo url:        $_url"
     notify "    repo branch:     $_branch"
 
-    if [ ! -d "$_dest" ]; then notify "cloning $url to $_dest"; fi;
-    if [ ! -d "$_dest" ];
-        # NB not using depth, or cloning the branch directly, b/c it doesn't work well w/ tags
+    if [ ! -d "$_dest" ]; then notify "cloning $url to $_dest"; fi
+    if [ ! -d "$_dest" ]; then # NB not using depth, or cloning the branch directly, b/c it doesn't work well w/ tags
         # it gets into a weird situation where you can't fetch tags, (couldn't find remote ref refs/heads/...)
         # and then you can't checkout that tag b/c it doesn't exist
-        then git clone --recurse-submodules $_url $_dest \
-            && git fetch origin --tags --force \
-            && git -C $_dest checkout $_branch \
-            && git -C $_dest submodule update --init --recursive;
-    fi;
+        git clone --recurse-submodules $_url $_dest &&
+            git -C $_dest fetch origin --tags --force &&
+            git -C $_dest checkout $_branch &&
+            git -C $_dest submodule update --init --recursive
+    fi
     # NB `git status` updates the .git folder for some reason,
     #    so don't call it here or else it will update even if there are no changes!
 
@@ -72,7 +66,7 @@ update_repo_to_master () {
     # TODO may need to use {branch}^{} for tags, else they point to the wrong commit
     _remote=$(git -C $_dest ls-remote origin refs/heads/${_branch} | cut -f1)
     if [ -z "$_remote" ]; then
-    # dereference b/c we need the OID of the commit associated w/ the tag, not the object ID of the tag itself
+        # dereference b/c we need the OID of the commit associated w/ the tag, not the object ID of the tag itself
         _remote=$(git -C $_dest rev-parse refs/tags/${_branch}^{})
     fi
     if [ -z "$_remote" ]; then
@@ -99,48 +93,47 @@ update_repo_to_master () {
     echo ""
 }
 
+are_packages_missing() {
+    # print warning if any package in array $1 is not installed
 
-are_packages_missing () {
-	# print warning if any package in array $1 is not installed
+    local _pkgs=("$@")
+    local _missing=()
 
-	local _pkgs=( "$@" )
-	local _missing=()
+    for pkg in ${_pkgs[@]}; do
+        dpkg -s $pkg 2>/dev/null | grep -q "install ok installed" || _missing+=("$pkg")
+    done
+    if [ ${#_missing[@]} -eq 0 ]; then
+        return 0
+    fi
 
-	for pkg in ${_pkgs[@]}; do \
-		dpkg -s $pkg 2>/dev/null | grep -q "install ok installed" || _missing+=("$pkg")
-	done
-	if [ ${#_missing[@]} -eq 0 ]; then
-		return 0
-	fi
-
-	error "you are missing packages!"
-	error "    some targets may not succeed,"
-	error "    or some important programs might be missing"
-	error "please install:"
-	echo ""
-	echo "  ${_missing[@]}"
-	echo ""
-	sleep 4
-	return 1
+    error "you are missing packages!"
+    error "    some targets may not succeed,"
+    error "    or some important programs might be missing"
+    error "please install:"
+    echo ""
+    echo "  ${_missing[@]}"
+    echo ""
+    sleep 4
+    return 1
 }
 
-are_packages_missing_warn () {
-	# print warning if any package in array $1 is not installed
+are_packages_missing_warn() {
+    # print warning if any package in array $1 is not installed
 
-	local _pkgs=( "$@" )
-	local _missing=()
+    local _pkgs=("$@")
+    local _missing=()
 
-	for pkg in ${_pkgs[@]}; do \
-		dpkg -s $pkg 2>/dev/null | grep -q "install ok installed" || _missing+=("$pkg")
-	done
-	if [ ${#_missing[@]} -eq 0 ]; then
-		return 0
-	fi
+    for pkg in ${_pkgs[@]}; do
+        dpkg -s $pkg 2>/dev/null | grep -q "install ok installed" || _missing+=("$pkg")
+    done
+    if [ ${#_missing[@]} -eq 0 ]; then
+        return 0
+    fi
 
-	warn "missing package, the following is recommended:"
-	echo ""
-	echo "  ${_missing[@]}"
-	echo ""
-	sleep 4
-	return 1
+    warn "missing package, the following is recommended:"
+    echo ""
+    echo "  ${_missing[@]}"
+    echo ""
+    sleep 4
+    return 1
 }
