9c1178946bbfdf47d1ef84ed640b812a4ef531ac
Michael Teresi <miketeresi@gmail.com>
2026-02-23 21:49:16 -0500

    update ncurses
    - update ncurses makefile to use new functions
    - update ncurses from 6.5 -> 6.6
    - add Make to treesitter parsers
    - add foldlevel back to treesitter config on window open,
      needed to get starting fold level back to max

--- assets/nvim/lua/plugins/treesitter.lua
+++ assets/nvim/lua/plugins/treesitter.lua
@@ -11,6 +11,7 @@ return { -- Highlight, edit, and navigate code
 			"html",
 			"lua",
 			"luadoc",
+			"make",
 			"markdown",
 			"markdown_inline",
 			"query",
@@ -23,6 +24,11 @@ return { -- Highlight, edit, and navigate code
 		-- NB: make sure to install tree-sitter-cli (cargo install tree-sitter-cli)
 		ts.setup({})
 		ts.install(languages)
+
+		-- NOTE:: work in progress, investigating broken highlights
+		--ts.setup({
+		--	additional_vim_regex_highlighting = false,
+		--})
 
 		-- SEE: https://mhpark.me/posts/update-treesitter-main/
 		-- Not every tree-sitter parser is the same as the file type detected
@@ -47,6 +53,8 @@ return { -- Highlight, edit, and navigate code
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 				vim.wo.foldtext = "" -- show the line, not a folding message
 				vim.wo.foldcolumn = "0" -- number chars on lefthand side to display fold level
+				vim.wo.foldlevel = 005 -- starting fold (0 means all levels are folded)
+				vim.wo.foldnestmax = 5 -- max level to close, treat as one chunk
 
 				-- Enable treesitter-based indentation
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
